7d92de4f2cfb0c501e67c67f9d717a441dbfab82
chungphing <chungphing@gmail.com>
2019-05-28 17:31:21 +0700

    add eslint config

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-//include nessesary file
-import models from'./models/index';
-//const { connectDb } = require('./models/index');
-
-console.log(models);
+// include nessesary file
 import mongoose from 'mongoose';
 
-import http from 'http'
-import express from 'express'
-import path from 'path'
-const app = express();
+import http from 'http';
+import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import uuid from 'uuid';
+import session from 'express-session';
+import models from './models/index';
+// const { connectDb } = require('./models/index');
+
+console.log(models);
+const app = express();
 
 app.disable('x-powered-by');
 
-//configure application
-const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+// configure application
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+
 app.engine('handlebars', handlebars.engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3001);
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname + '/public');
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use(express.static(`${__dirname}/public`));
+console.log(`${__dirname}/public`);
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true,
 }));
 
-//data
-//var {items} =  require('./data/data.json')
+// data
+// var {items} =  require('./data/data.json')
 
-//routes
-//const about = require('./routes/about')
+// routes
+// const about = require('./routes/about')
 // const contact = require('./routes/contact')
 // const deals = require('./routes/deals')
 // const all = require('./routes/all')
 // const post = require('./routes/post')
 // const lists = require('./routes/lists');
 // const product = require('./routes/product')
-app.get('/', (req,res)=> {
-  res.render('index', {active: {home: true}})
- })
-//app.use('/about', about)
+app.get('/', (req, res) => {
+  res.render('index', { active: { home: true } });
+});
+// app.use('/about', about)
 // app.use('/contact', contact)
 // app.use('/deals', deals)
 // app.use('/all', all)
 // app.use('/post', post)
 // app.use('/product', product)
-//app.use('/product/:id', product)
+// app.use('/product/:id', product)
 
 // app.get('/product/:id', function(request, response, next) {
 //   var id = request.params.id;
@@ -57,18 +60,17 @@ app.get('/', (req,res)=> {
 // app.use('/lists', lists)
 
 
-app.use(function(req, res){
-  console.log("Error 404");
+app.use((req, res) => {
+  console.log('Error 404');
   res.type('text/html');
   res.status(404);
   res.render('404');
 });
-app.use(function(err, req, res, next){
+app.use((err, req, res, next) => {
   res.type('text/html');
   res.status(500);
   res.render('500');
-  console.log("Error 500");
-
+  console.log('Error 500');
 });
 
 
@@ -79,9 +81,8 @@ app.use(function(err, req, res, next){
 //   });
 
 
-  mongoose.connect('mongodb://localhost:27017/web-arch').then(async () => {
-    app.listen(app.get('port'), () => {
-        console.log("listen on port", app.get('port'));
-    })
+mongoose.connect('mongodb://localhost:27017/web-arch').then(async () => {
+  app.listen(app.get('port'), () => {
+    console.log('listen on port', app.get('port'));
   });
-
+});
